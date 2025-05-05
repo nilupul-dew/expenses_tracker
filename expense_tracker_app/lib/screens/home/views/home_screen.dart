3a8a076d845f5a0f90dfd70d35b06cd79f5784a9
W.A.S.C.N. Dewmini Ariyarathne <nilupul1025@gmail.com>
2025-05-05 08:26:33 +0530

    stateless to stateful
    converted the stateless widget in the home screen into stateful and added dependencies for pubspec.yaml file for the graphs in next screen

--- expense_tracker_app/lib/screens/home/views/home_screen.dart
+++ expense_tracker_app/lib/screens/home/views/home_screen.dart
@@ -4,9 +4,14 @@ import 'package:expense_tracker_app/screens/home/views/main_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
